a2588a369b84b010087214b764f26f7ae10f6967
Neda Ara <neda.ara9@gmail.com>
2025-07-06 23:04:48 +0530

    Add DB and Inngest setup

--- components/header.tsx
+++ components/header.tsx
@@ -40,7 +40,7 @@ export const Header = () => {
             </Link>
 
             <DropdownMenu>
-              <DropdownMenuTrigger>
+              <DropdownMenuTrigger asChild>
                 <Button>
                   <StarsIcon className="h-4 w-4" />
                   <span className="hidden sm:inline">Growth Tools</span>
---
+++ lib/inngest/client.ts
@@ -0,0 +1,6 @@
+import { Inngest } from "inngest";
+
+export const inngest = new Inngest({
+  id: "karo-hire.ai",
+  name: "KairoHire.ai",
+});
